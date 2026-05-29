08766744091f42f92a0230a500a79959f45c043d
nrzds666 <nrzds666@users.noreply.github.com>
2026-05-29 16:35:37 +0800

    fix: switch to Vite5, fix element-plus locale import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
@@ -10,5 +9,5 @@ import './assets/style.css'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus, { locale: zhCn })
+app.use(ElementPlus)
 app.mount('#app')
